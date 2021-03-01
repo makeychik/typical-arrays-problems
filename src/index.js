a4c01f961da96b9fdc5f06af13061bb5d95ddaf2
makeychik <mikitamakeichyk@gmail.com>
2021-03-02 00:31:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((minEl, curr) => (minEl < curr ? minEl : curr));
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((maxEl, curr) => (maxEl > curr ? maxEl : curr));
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((sum, curr) => (sum + curr)) / array.length;
+};
